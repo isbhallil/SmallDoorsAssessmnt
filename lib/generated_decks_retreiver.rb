e4cde938af6852ec65dba6f38bb1ec6b0cc0161e
ishaq bhallil <is.bhallil@gmail.com>
2021-09-19 20:51:19 -0400

    added GeneratedDecksRetreiver and boilerplate tests

---
+++ lib/generated_decks_retreiver.rb
@@ -0,0 +1,7 @@
+require "application_service"
+
+class GeneratedDecksRetreiver < ApplicationService
+  def call
+    p "[GeneratedDecksRetreiver] Hello World !"
+  end
+end
---
+++ spec/generated_decks_retreiver_spec.rb
@@ -0,0 +1,6 @@
+require "generated_decks_retreiver_spec"
+
+describe GeneratedDecksRetreiver do
+  it "should return previously generated decks" do
+  end
+end
